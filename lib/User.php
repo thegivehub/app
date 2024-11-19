7649c96bbcdc1c6beb04db9bf43eeee2416e94f4
Christopher Robison <cdr@netoasis.net>
2024-11-19 13:58:50 -0800

    Added findId method to find ids

--- lib/User.php
+++ lib/User.php
@@ -17,6 +17,27 @@ class User {
         return $this->get($id);
     }
 
+    public function findId($filterArr=null) {
+        if ($filterArr) {
+            // print_r($filterArr);
+            $all = $this->collection->find($filterArr)->toArray();
+
+            if ($all) {
+                return $all[0]->_id;
+            }
+        } else {
+            return [];
+        }         
+    }
+
+    public function find($filterArr=null) {
+        if ($filterArr) {
+            return $this->collection->find($filterArr)->toArray();
+        } else {
+            return $this->collection->find()->toArray();
+        }
+    }
+    
     public function get($id = null) {
         if ($id) {
             return $this->collection->findOne(['_id' => new MongoDB\BSON\ObjectId($id)]);
