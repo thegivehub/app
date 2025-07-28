4dc298314fb3ebc61aa666ca6b45c4222057bc70
Christopher Robison <cdr@netoasis.net>
2025-07-28 13:01:10 -0700

    Improve offline fallback

--- service-worker.js
+++ service-worker.js
@@ -18,7 +18,7 @@ self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(keys =>
             Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-        )
+        ).then(() => self.clients.claim())
     );
 });
 
